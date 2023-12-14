17dd5dbad7eea6671197d103db7983d0dc2875d2
Jason MAHOUX <jasonmahoux@gmail.com>
2023-12-14 11:18:54 +0100

    Terraform azure work

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,7 @@
-FROM eclipse-temurin:17-jdk-alpine
-ENTRYPOINT ["java","-jar","/app.jar"]
+FROM openjdk:17
+WORKDIR /
+ADD target/*.jar app.jar
+RUN useradd -m myuser
+USER myuser
+EXPOSE 80
+CMD java -jar -Dspring.profiles.active=prod app.jar
--- src/main/resources/terraform/main.tf
+++ src/main/resources/terraform/main.tf
@@ -1,8 +1,14 @@
-/*terraform init
-terraform plan
-terraform apply
-terraform destroy*/
+// Before that a docker image was created with the app .jar build with the maven install  -Pproduction mode
 
+// first download terraform in https://www.terraform.io/
+// terraform init
+// terraform plan
+// terraform apply to create all the resources
+// terraform destroy to delete all the resources created
+
+/*
+* Resource 1
+*/
 provider "azurerm" {
     features {}
 
@@ -13,19 +19,18 @@ provider "azurerm" {
     skip_provider_registration = true
 }
 
-data "azurerm_client_config" "current" {
-
-}
-
 resource "azurerm_resource_group" "gestionfacture" {
     name     = "gestion_facture"
     location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "serviceplan" {
+resource "azurerm_app_service_plan" "gestionfactureserviceplan" {
     name                = "gestion_facture-serviceplan"
     location            = azurerm_resource_group.gestionfacture.location
     resource_group_name = azurerm_resource_group.gestionfacture.name
+
+    kind = "Linux"
+    reserved = true
 
     sku {
         tier = "Standard"
@@ -33,22 +38,20 @@ resource "azurerm_app_service_plan" "serviceplan" {
     }
 }
 
-resource "azurerm_app_service" "example" {
+resource "azurerm_app_service" "gestionfactureservice" {
     name                = "gestion-facture-jmx"
     location            = azurerm_resource_group.gestionfacture.location
     resource_group_name = azurerm_resource_group.gestionfacture.name
-    app_service_plan_id = azurerm_app_service_plan.serviceplan.id
+    app_service_plan_id = azurerm_app_service_plan.gestionfactureserviceplan.id
 
     site_config {
-        java_version = "11"
-        scm_type     = "LocalGit"
-        linux_fx_version = "DOCKER|"
+        linux_fx_version = "DOCKER|jasonmahoux/gestion-facture-jmx:latest_26"
     }
 
     connection_string {
         name  = "Database"
         type  = "PostgreSQL"
-        value = "Server=gestionfacture-server.postgres.database.azure.com;Integrated Security=SSPI"
+        value = "Server=postgresql-server-jmx.postgres.database.azure.com;Integrated Security=SSPI"
     }
 }
 
@@ -72,4 +75,62 @@ resource "azurerm_postgresql_database" "gestionfacturedatabase" {
     collation           = "English_United States.1252"
 }
 
+/*
+* Resource 2
+*/
+resource "azurerm_resource_group" "gestionfacturebis" {
+    name     = "gestion_facture_bis"
+    location = "France Central"
+}
 
+resource "azurerm_app_service_plan" "gestionfactureserviceplanbis" {
+    name                = "gestion_facture-serviceplan-bis"
+    location            = azurerm_resource_group.gestionfacturebis.location
+    resource_group_name = azurerm_resource_group.gestionfacturebis.name
+
+    kind = "Linux"
+    reserved = true
+
+    sku {
+        tier = "Standard"
+        size = "S1"
+    }
+}
+
+resource "azurerm_app_service" "gestionfactureservicebis" {
+    name                = "gestion-facture-jmx-bis"
+    location            = azurerm_resource_group.gestionfacturebis.location
+    resource_group_name = azurerm_resource_group.gestionfacturebis.name
+    app_service_plan_id = azurerm_app_service_plan.gestionfactureserviceplanbis.id
+
+    site_config {
+        linux_fx_version = "DOCKER|jasonmahoux/gestion-facture-jmx:latest_26"
+    }
+
+    connection_string {
+        name  = "Database"
+        type  = "PostgreSQL"
+        value = "Server=postgresql-server-jmx-bis.postgres.database.azure.com;Integrated Security=SSPI"
+    }
+}
+
+// TODO En Version payante faire un replica de la base de données de la ressource 1 vers la ressource 2
+resource "azurerm_postgresql_server" "databaseserverbis" {
+    location                = azurerm_resource_group.gestionfacturebis.location
+    name                    = "postgresql-server-jmx-bis"
+    resource_group_name     = azurerm_resource_group.gestionfacturebis.name
+    sku_name                = "B_Gen5_2"
+    ssl_enforcement_enabled = false
+    ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
+    version                 = "9.5"
+    administrator_login     = "postgres"
+    administrator_login_password = "PgSqL#24!01$19"
+}
+
+resource "azurerm_postgresql_database" "gestionfacturedatabasebis" {
+    name                = "gestion-facture-database-jmx-bis"
+    resource_group_name = azurerm_resource_group.gestionfacturebis.name
+    server_name         = azurerm_postgresql_server.databaseserverbis.name
+    charset             = "UTF8"
+    collation           = "English_United States.1252"
+}
